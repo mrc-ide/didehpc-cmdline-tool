9e68dd6058443a94ec25a1ee9a76649c91f838eb
Wes Hinsley <w.hinsley@imperial.ac.uk>
2023-05-21 17:09:00 +0100

    /b64 tag for servers, and add QueryDeps

--- Program.cs
+++ Program.cs
@@ -5,28 +5,51 @@ namespace didehpc
 {
     public partial class Didehpc
     {
+        bool b64 = false;
+        
         public Didehpc()
         {
         }
         private int Unknown_command(string command)
         {
+            Console.WriteLine("didehpc v0.9");
             Console.WriteLine("Command " + command + " not found.");
-            Console.WriteLine("Commands: bump, cancel, ldap, list, shunt, waitfor");
+            Console.WriteLine("Commands: bump      -  Change priority of multiple jobs");
+            Console.WriteLine("          cancel    -  Cancel multiple jobs");
+            Console.WriteLine("          ldap      -  Talk to LDAP server about user groups");
+            Console.WriteLine("          list      -  List jobs in various ways");
+            Console.WriteLine("          querydeps -  List states of parent jobs being waited on");
+            Console.WriteLine("          shunt     -  Move jobs to different template");
+            Console.WriteLine("          submit    -  Submit multiple jobs");
+            Console.WriteLine("          waitfor   -  Block until a job finishes");
             return 1;
         }
 
         public static void Main(string[] args)
         {
             Didehpc D = new Didehpc();
+            if ((args.Length >= 1) && (args[0].ToUpper().Equals("/B64")))
+            {
+                D.b64 = true;
+                string[] args2 = new string[args.Length - 1];
+                for (int i = 1; i < args.Length; i++)
+                {
+                    args2[i - 1] = args[i];
+                }
+                args = args2;
+            }
+
             string command = (args.Length >= 1) ? args[0].ToLower() : "<none>";
             Environment.Exit(
-                command == "bump"     ? D.Bump_jobs(args) :
-                command == "cancel"   ? D.Cancel_jobs(args) :
-                command == "ldap"     ? D.Ldap_madness(args) :
-                command == "list"     ? D.List_jobs(args) :
-                command == "shunt"    ? D.Shunt_jobs(args) :
-                command == "waitfor"  ? D.Wait_for(args) :
-                                        D.Unknown_command(command));
+                command == "bump"      ? D.Bump_jobs(args) :
+                command == "cancel"    ? D.Cancel_jobs(args) :
+                command == "ldap"      ? D.Ldap_madness(args) :
+                command == "list"      ? D.List_jobs(args) :
+                command == "querydeps" ? D.Query_Deps(args) :
+                command == "shunt"     ? D.Shunt_jobs(args) :
+                command == "submit"    ? D.Submit_jobs(args) :
+                command == "waitfor"   ? D.Wait_for(args) :
+                                         D.Unknown_command(command));;
         }
     }
 }
---
+++ QueryDeps.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.Hpc.Scheduler;
+
+namespace didehpc
+{
+    public partial class Didehpc
+    {
+
+        private int Query_Deps_message_exit()
+        {
+            Console.WriteLine("Syntax: didehpc querypdeps scheduler id");
+            return 1;
+        }
+        
+        private int Query_Deps(string[] args)
+        {
+            int argc = args.Length;
+            if (argc != 3)
+            {
+                return(Query_Deps_message_exit());
+            }
+
+            if (b64)
+            {
+                args[1] = Base64_decode(args[1]);
+                args[2] = Base64_decode(args[2]);
+            }
+            
+            string scheduler_name = (args[1].Equals(".")) ? System.Environment.MachineName : args[1];
+            int job_id_int = -1;
+            int.TryParse(args[2], out job_id_int);
+            
+            IScheduler scheduler = Get_scheduler(scheduler_name);
+            ISchedulerJob scheduler_job = Get_job(scheduler, job_id_int);
+
+            if (scheduler_job == null)
+            {
+                Console.WriteLine(job_id_int + "\tNOT_FOUND\n");
+                return 1;
+            }
+
+            foreach (int parent_id in scheduler_job.ParentJobIds)
+            {
+                ISchedulerJob parent_job = Get_job(scheduler, parent_id);
+                if (parent_job != null)
+                {
+                    Console.WriteLine(parent_id + "\t" + parent_job.State);
+                }
+            }
+            scheduler.Close();
+            return 0;
+        }
+    }
+}
